e6b5da47118d589740f80e3ed1582ba0822cf0d1
an.je.gonz <lsteamer@gmail.com>
2025-05-26 12:19:26 +0200

    presentation- Area for LineChart defined: area + background done

---
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_detail/LineChart.kt
@@ -0,0 +1,170 @@
+package com.plcoding.cryptotracker.crypto.presentation.coin_detail
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.plcoding.cryptotracker.crypto.domain.CoinPrice
+import com.plcoding.cryptotracker.ui.theme.CryptoTrackerTheme
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import kotlin.random.Random
+
+@Composable
+fun LineChart(
+    dataPoints: List<DataPoint>,
+    style: ChartStyle,
+    visibleDataPointIndices: IntRange,
+    unit: String,
+    modifier: Modifier = Modifier,
+    selectedDataPoint: DataPoint? = null,
+    onDataPointSelected: (DataPoint) -> Unit = {},
+    onXLabelWithChange: (Float) -> Unit = {},
+    showHelperLines: Boolean = true
+) {
+    val textStyle = LocalTextStyle.current.copy(
+        fontSize = style.labelFontSize
+    )
+
+    val visibleDataPoints = remember(dataPoints, visibleDataPointIndices) {
+        dataPoints.slice(visibleDataPointIndices)
+    }
+
+    val maxYValue = remember(visibleDataPoints) {
+        visibleDataPoints.maxOfOrNull { it.y } ?: 0f
+    }
+
+    val minYValue = remember(visibleDataPoints) {
+        visibleDataPoints.minOfOrNull { it.y } ?: 0f
+    }
+
+    val measurer = rememberTextMeasurer()
+
+    val xLabelWidth by remember {
+        mutableFloatStateOf(0f)
+    }
+    LaunchedEffect(key1 = xLabelWidth) {
+        onXLabelWithChange(xLabelWidth)
+    }
+
+    val selectedDataPointIndex = remember(selectedDataPoint) {
+        dataPoints.indexOf(selectedDataPoint)
+    }
+
+    val drawPoints by remember {
+        mutableStateOf(listOf<DataPoint>())
+    }
+
+    val isShowingDataPoints by remember {
+        mutableStateOf(selectedDataPoint != null)
+    }
+
+    Canvas(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        val minLabelSpacingYPx = style.minYLabelSpacing.roundToPx()
+        val verticalPaddingPx = style.verticalPadding.roundToPx()
+        val horizontalPaddingPx = style.horizontalPadding.roundToPx()
+        val xAxisLabelSpacingPx = style.xAsisLabelSpacing.roundToPx()
+
+        val xLabelTextLayoutResults = visibleDataPoints.map {
+            measurer.measure(
+                text = it.xLabel,
+                style = textStyle.copy(textAlign = TextAlign.Center)
+            )
+        }
+
+        val maxXLabelWidth = xLabelTextLayoutResults.maxOfOrNull { it.size.width } ?: 0
+        val maxXLabelHeight = xLabelTextLayoutResults.maxOfOrNull { it.size.height } ?: 0
+        val maxXLabelLineCount = xLabelTextLayoutResults.maxOfOrNull { it.lineCount } ?: 0
+        val xLabelLineHeight = maxXLabelHeight / maxXLabelLineCount
+
+        val viewPortHeightPx = size.height -
+                (maxXLabelHeight + 2 * verticalPaddingPx + xLabelLineHeight + xAxisLabelSpacingPx)
+        val viewPortTopY = verticalPaddingPx + xLabelLineHeight + 10f
+        val viewPortRightX = size.width
+        val viewPortBottomY = viewPortTopY + viewPortHeightPx
+        val viewPortLeftX = 2f * horizontalPaddingPx
+        val viewPort = Rect(
+            left = viewPortLeftX,
+            top = viewPortTopY,
+            right = viewPortRightX,
+            bottom = viewPortBottomY
+        )
+
+        drawRect(
+            color = Color.Green,
+            topLeft = viewPort.topLeft,
+            size = viewPort.size
+        )
+
+    }
+}
+
+@PreviewLightDark
+@Composable
+fun LineChartPreview() {
+    CryptoTrackerTheme {
+        val coinHistoryRandomized = remember {
+            (1..20).map {
+                CoinPrice(
+                    priceUsd = Random.nextFloat() * 1000.0,
+                    dateTime = ZonedDateTime.now().plusHours(it.toLong())
+                )
+            }
+        }
+        val style = ChartStyle(
+            chartLineColor = Color.Black,
+            unselectedColor = Color(0xFF7C7C7C),
+            selectedColor = Color.Black,
+            helperLinesThicknessPx = 5f,
+            axisLinesThicknessPx = 5f,
+            labelFontSize = 14.sp,
+            minYLabelSpacing = 25.dp,
+            verticalPadding = 8.dp,
+            horizontalPadding = 8.dp,
+            xAsisLabelSpacing = 8.dp
+        )
+        val dataPoints = remember {
+            coinHistoryRandomized.map {
+                DataPoint(
+                    x = it.dateTime.hour.toFloat(),
+                    y = it.priceUsd.toFloat(),
+                    xLabel = DateTimeFormatter
+                        .ofPattern("ha\nM/d")
+                        .format(it.dateTime)
+                )
+            }
+        }
+        LineChart(
+            dataPoints  = dataPoints,
+            style = style,
+            visibleDataPointIndices = 0..19,
+            unit = "$",
+            modifier = Modifier
+                .width(700.dp)
+                .height(300.dp)
+                .background(Color.White),
+            selectedDataPoint = dataPoints[1]
+        )
+    }
+
+
+}
